2f010556c5862e3d4a1ee44c1f144f240542f743
sladumglas <sladumglas@gmail.com>
2026-01-13 23:39:48 +0900

    step1

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/js/index.js"),
+  output: {
+    filename: "js/bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "",
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 8080,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/main.css",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/image", to: "image" },
+        { from: "src/font", to: "font" },
+      ],
+    }),
+  ],
+};
