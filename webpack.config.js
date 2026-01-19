72019a2fe6a40bfdd984f0e20ef95c4aa482b8d4
sladumglas <sladumglas@gmail.com>
2026-01-19 19:38:41 +0900

    pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,60 +4,71 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = {
-  entry: path.resolve(__dirname, "src/js/index.js"),
-  output: {
-    filename: "js/bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "",
-  },
-  devServer: {
-    static: path.resolve(__dirname, "dist"),
-    port: 8080,
-    open: true,
-    hot: true,
-  },
+module.exports = (env, argv) => {
+  const isProd = argv.mode === "production";
+  const repoName = "your-project-name-web-core-9.6";
 
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
-      },
+  return {
+    entry: path.resolve(__dirname, "src/js/index.js"),
 
-      {
-        test: /\.scss$/i,
-        use: [
-          {
-          loader: MiniCssExtractPlugin.loader,
-          options: { publicPath: "../"},
-          },
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
-        ],
-      },
+    output: {
+      filename: "js/bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      publicPath: isProd ? `/${repoName}/` : "",
+    },
 
-      {
-        test: /\.(woff2?|ttf|eot|otf)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "font/[name][ext]",
+    devServer: {
+      static: path.resolve(__dirname, "dist"),
+      port: 8080,
+      open: true,
+      hot: true,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            { loader: MiniCssExtractPlugin.loader, options: { publicPath: "../" } },
+            "css-loader",
+            "postcss-loader",
+          ],
         },
-      },
-    ],
-  },
 
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/index.html"),
-    }),
-    new MiniCssExtractPlugin({
-      filename: "css/main.css",
-    }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: "src/image", to: "image" }],
-    }),
-  ],
-};
+        {
+          test: /\.scss$/i,
+          use: [
+            { loader: MiniCssExtractPlugin.loader, options: { publicPath: "../" } },
+            "css-loader",
+            "postcss-loader",
+            "sass-loader",
+          ],
+        },
+
+        {
+          test: /\.(woff2?|ttf|eot|otf)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "font/[name][ext]",
+          },
+        },
+      ],
+    },
+
+    plugins: [
+      new CleanWebpackPlugin(),
+
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src/index.html"),
+      }),
+
+      new MiniCssExtractPlugin({
+        filename: "css/main.css",
+      }),
+
+      new CopyWebpackPlugin({
+        patterns: [{ from: "src/image", to: "image" }],
+      }),
+    ],
+  };
+}
